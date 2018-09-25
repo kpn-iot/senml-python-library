9a458f00e8d9a77bdff73c893262605c741b76be
Uilian Ries <uilian@khomp.com>
2018-09-25 12:03:58 -0300

    #1 Update setup description
    Signed-off-by: Uilian Ries <uilian@khomp.com>

--- setup.py
+++ setup.py
@@ -2,16 +2,25 @@ from setuptools import setup
 
 setup(
     name='kpn_senml',
-    version='1.0.1',
+    version='1.1.0',
     packages=['kpn_senml'],
     url='https://kpn-iot.github.io/senml-python-library/',
-    license='APACHE',
+    license='MIT',
     author='Jan Bogaerts',
     author_email='jb@elastetic.com',
     description='generate and parse senml json and cbor data',
     long_description='With this library you can generate senml packs containing sensor data in both json and cbor format.  It can also parse senml data in both json and cbor format in order to support actuators.',
+    classifiers=[
+            'Development Status :: 5 - Production/Stable',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Embedded Systems',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 2',
+            'Programming Language :: Python :: 2.7',
+            'Operating System :: OS Independent',
+    ],
     keywords='senml kpn cbor json',
     install_requires=[
-          'cbor2',
+          'cbor2==4.1.0',
     ]
 )
